b3308da996a5e15810334c830b2c92f7495b2fb2
Harsh Singh <jnp.ranjanasingh@gmail.com>
2025-10-04 23:31:46 +0530

    Resolved some merge conflict.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,39 +1,28 @@
 import { useState } from 'react'
-
-import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import MainLayout from './Layouts/MainLayout';
 import Home from './Pages/Home';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import MedicineForm from './Pages/MedicineForm';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CircusLandingPage from './Components/landing/CircusLandingPage.jsx';
-import './App.css';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <MainLayout sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/medicine-form" element={<MedicineForm />} />
-            {/* Add more routes as needed */}
-          </Routes>
-        </MainLayout>
-  )
     <Router>
-      <Routes>
-        <Route path="/" element={<CircusLandingPage />} />
-        {/* Add more routes here as you build */}
-        <Route path="/dashboard" element={<div>Dashboard Coming Soon</div>} />
-        <Route path="/login" element={<div>Login Page Coming Soon</div>} />
-        <Route path="/signup" element={<div>Signup Page Coming Soon</div>} />
-      </Routes>
+      <MainLayout sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/circus" element={<CircusLandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/medicine-form" element={<MedicineForm />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </MainLayout>
     </Router>
   );
 }
