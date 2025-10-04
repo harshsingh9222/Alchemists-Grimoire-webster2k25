bc49fa1508523450d551c9b89af4484d1538cce5
dhruvordangi <dhruvdangi03@gmail.com>
2025-10-04 22:46:43 +0530

    otp login created

---
+++ Server/src/Utils/googleConfig.js
@@ -0,0 +1,13 @@
+import { google } from 'googleapis';
+
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+console.log("GOOGLE_REDIRECT_URI:", process.env.GOOGLE_REDIRECT_URI);
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+);
+
+export default oauth2Client;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,11 +1,11 @@
-import dotenv from "dotenv";
+// Load environment variables as early as possible so other modules (which may read
+// env vars at import time) see them.
+import 'dotenv/config';
 // import cors from 'cors';
 import { app } from "./app.js";
 import { connectDB } from "./DB/connectDB.js";
 import userRouter from './Routes/user.Routes.js'
 
-
-dotenv.config({ path: '.env' });
 console.log("Loaded MONGODB_URI =", process.env.MONGODB_URI);
 
 
